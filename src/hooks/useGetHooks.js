13ccd19ae38141979c74a068adddd5402ce12eba
Vadim Egorov <43100771+vadimegorov13@users.noreply.github.com>
2022-03-27 06:52:19 -0800

    get hooks

--- src/hooks/useGetHooks.js
+++ src/hooks/useGetHooks.js
@@ -1,7 +1,6 @@
-import {db} from "../firebase/firebase"
+import { db } from '../firebase/firebase';
 
 export const useGetHooks = () => {
-
   const getIncluded = async (userId, setIngredients) => {
     if (userId) {
       try {
@@ -13,7 +12,101 @@ export const useGetHooks = () => {
             const data = response.data();
             setIngredients(data.ingredients);
           });
+      } catch (error) {
+        console.log("Something went wrong, here's an error:", error);
+      }
+    } else {
+      console.log('User is not authenticated');
+    }
+  };
 
+  const getExcluded = async (userId, setIngredients) => {
+    if (userId) {
+      try {
+        await db
+          .collection('excludedIngredients')
+          .doc(userId)
+          .get()
+          .then((response) => {
+            const data = response.data();
+            setIngredients(data.ingredients);
+          });
+      } catch (error) {
+        console.log("Something went wrong, here's an error:", error);
+      }
+    } else {
+      console.log('User is not authenticated');
+    }
+  };
+
+  const getHealth = async (userId, setHealth) => {
+    if (userId) {
+      try {
+        await db
+          .collection('health')
+          .doc(userId)
+          .get()
+          .then((response) => {
+            const data = response.data();
+            setHealth(data.health);
+          });
+      } catch (error) {
+        console.log("Something went wrong, here's an error:", error);
+      }
+    } else {
+      console.log('User is not authenticated');
+    }
+  };
+
+  const getDiet = async (userId, setDiet) => {
+    if (userId) {
+      try {
+        await db
+          .collection('diet')
+          .doc(userId)
+          .get()
+          .then((response) => {
+            const data = response.data();
+            setDiet(data.health);
+          });
+      } catch (error) {
+        console.log("Something went wrong, here's an error:", error);
+      }
+    } else {
+      console.log('User is not authenticated');
+    }
+  };
+
+  const getHistory = async (userId, setHistory) => {
+    if (userId) {
+      try {
+        await db
+          .collection('history')
+          .doc(userId)
+          .get()
+          .then((response) => {
+            const data = response.data();
+            setHistory(data.links);
+          });
+      } catch (error) {
+        console.log("Something went wrong, here's an error:", error);
+      }
+    } else {
+      console.log('User is not authenticated');
+    }
+  };
+
+  const getFavorite = async (userId, setFavorite) => {
+    if (userId) {
+      try {
+        await db
+          .collection('favorite')
+          .doc(userId)
+          .get()
+          .then((response) => {
+            const data = response.data();
+            setFavorite(data.links);
+          });
       } catch (error) {
         console.log("Something went wrong, here's an error:", error);
       }
@@ -24,5 +117,10 @@ export const useGetHooks = () => {
 
   return {
     getIncluded,
+    getExcluded,
+    getHealth,
+    getDiet,
+    getHistory,
+    getFavorite,
   };
 };
